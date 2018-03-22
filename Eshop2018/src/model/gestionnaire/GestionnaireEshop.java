6458c2eb92f0ab3c03ad3fdba74d2f8109a8e307
Fabian Amel <amelfabian@gmail.com>
2018-03-22 19:29:05 +0100

    persistence

--- Eshop2018/src/model/gestionnaire/GestionnaireEshop.java
+++ Eshop2018/src/model/gestionnaire/GestionnaireEshop.java
@@ -1,7 +1,19 @@
 package model.gestionnaire;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+@ManagedBean
+@ApplicationScoped
 public class GestionnaireEshop {
 	
+	private EntityManager em;
 	
+	public GestionnaireEshop() {
+		super();
+		this.em = Persistence.createEntityManagerFactory()
+	}
 
 }
