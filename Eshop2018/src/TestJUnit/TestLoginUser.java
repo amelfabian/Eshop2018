6cd708be1c6790aa33b666cfc5eab54ce2a3d480
Fabian Amel <amelfabian@gmail.com>
2018-03-22 20:02:16 +0100

    ORM

---
+++ Eshop2018/src/TestJUnit/TestLoginUser.java
@@ -0,0 +1,14 @@
+package TestJUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestLoginUser {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
